dbdde47f01509a160b11f2623fc1bc0ff4946068
pimentelraya-sketch <pimentelraya@gmail.com>
2025-12-23 20:24:27 -0600

    Add MainActivity with WebView for local content
    Implement WebView to load local HTML file.

---
+++ app/src/main/java/com/continental/app/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.continental.app
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.webViewClient = WebViewClient()
+
+        val s: WebSettings = webView.settings
+        s.javaScriptEnabled = true
+        s.domStorageEnabled = true
+        s.allowFileAccess = true
+        s.allowContentAccess = true
+        s.loadWithOverviewMode = true
+        s.useWideViewPort = true
+
+        // 100% offline: carga desde assets
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
